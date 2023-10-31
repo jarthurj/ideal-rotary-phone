a44348f91bb362975cfc1f61580cc76aa8f41791
jarthurj <jaoldaker1@gmail.com>
2023-10-30 22:01:33 -0400

    bootstrap working

--- apps/card_search/forms.py
+++ apps/card_search/forms.py
@@ -62,31 +62,31 @@ GAME_TYPES=(
 class CardSearch(forms.Form):
 	card_name = forms.CharField(max_length=50, 
 				required=False,
-				widget=forms.TextInput(attrs={'class':'form-control',
-						'placeholder':'Any words in the name ex."Fire"'}),
+				widget=forms.TextInput(attrs={'class':'form-control mb-3'
+					,'placeholder':'Any words in the name ex."Fire"'}),
 				label="Card Name:")
 
 	any_text = forms.CharField(max_length=50,
 				required=False,
 				label="Text:",
-				widget=forms.TextInput(attrs={'class':'form-control',
-						'placeholder':'Any text on the card ex."Draw a card"'}))
+				widget=forms.TextInput({'class':'form-control mb-3'
+					,'placeholder':'Any text on the card ex."Draw a card"'}))
 
 	colors = forms.MultipleChoiceField(
-				widget=forms.CheckboxSelectMultiple(attrs={'class':'form-control'}),
+				widget=forms.CheckboxSelectMultiple(),
 				choices=COLOR_CHOICES,required=False,
 				label="Colors:")
 
 
 	exactly_or_not = forms.MultipleChoiceField(
-				widget=forms.SelectMultiple(attrs={'class':'form-control'}),
+				widget=forms.SelectMultiple(),
 				choices=EXACT,
 				required=False,
 				initial=EXACT[3],
 				label="")
 
 	colors_identity = forms.MultipleChoiceField(
-				widget=forms.CheckboxSelectMultiple(attrs={'class':''}),
+				widget=forms.CheckboxSelectMultiple(attrs={}),
 				choices=COLOR_CHOICES,required=False,
 				label="Color Identity:")
 
@@ -97,32 +97,32 @@ class CardSearch(forms.Form):
 
 	#mpt = mana power toughness
 	mpt = forms.MultipleChoiceField(
-				widget=forms.Select,
+				widget=forms.Select(attrs={'class':'form-select'}),
 				choices=MPT,required=False,
 				label="Stats:")
 
 	mpt_condition = forms.MultipleChoiceField(
-				widget=forms.Select,
+				widget=forms.Select(attrs={'class':'form-select'}),
 				choices=MPTCONS,required=False,
 				label="")
 
 	mpt_parameter = forms.CharField(max_length=10, required=False, 
 		label="",
-		widget=forms.TextInput(attrs={'class':'form-control',
-						'placeholder':'ex. 6'}))
+		widget=forms.TextInput(attrs={'class':'form-control mb-3'
+					,'placeholder':'ex. 6'}))
 
 	lrb = forms.MultipleChoiceField(
-				widget=forms.Select,
+				widget=forms.Select(attrs={'class':'form-select'}),
 				choices=LRB,required=False,
 				label="Formats:")
 
 	game_types = forms.MultipleChoiceField(
-				widget=forms.Select,
+				widget=forms.Select(attrs={'class':'form-select'}),
 				choices=GAME_TYPES,required=False,
 				label="")
 
 	type_line = forms.CharField(max_length=50,required=False,
 		label="Type Line:",
-		widget=forms.TextInput(attrs={'class':'form-control',
-						'placeholder':'ex."Creature"'}))
+		widget=forms.TextInput(attrs={'class':'form-control mb-3'
+					,'placeholder':'ex."Creature"'}))
 	
